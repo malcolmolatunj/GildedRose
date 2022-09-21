edd5e608d3a71397d94892fc0969314561e01491
YoungTchalla18 <jolatunj@gmail.com>
2022-09-20 20:55:36 -0400

    switched negative conditionals with an 'else' clause to positive conditionals. assuming that assertions are easier to understand than negations.

--- gilded-rose.ps1
+++ gilded-rose.ps1
@@ -26,19 +26,19 @@ foreach ($item in $items) {
     $item.sellIn = $item.sellIn - 1
   }
   if ($item.sellIn -lt 0) {
-    if ($item.name -ne 'Aged Brie') {
-      if ($item.name -ne 'Backstage passes to a TAFKAL80ETC concert') {
+    if ($item.name -eq 'Aged Brie') {
+      if ($item.quality -lt 50) {
+        $item.quality = $item.quality + 1
+      }
+    } else {
+      if ($item.name -eq 'Backstage passes to a TAFKAL80ETC concert') {
+        $item.quality = $item.quality - $item.quality
+      } else {
         if ($item.quality -gt 0) {
           if ($item.name -ne 'Sulfuras, Hand of Ragnaros') {
             $item.quality = $item.quality - 1
           }
         }
-      } else {
-        $item.quality = $item.quality - $item.quality
-      }
-    } else {
-      if ($item.quality -lt 50) {
-        $item.quality = $item.quality + 1
       }
     }
   }
