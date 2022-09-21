d45566be40d7ca74068600a8a184655c31129b94
YoungTchalla18 <jolatunj@gmail.com>
2022-09-20 20:35:44 -0400

    flattened most ifs

--- gilded-rose.ps1
+++ gilded-rose.ps1
@@ -7,20 +7,14 @@ foreach ($item in $items) {
     -and $item.quality -gt 0
   ) {
     $item.quality = $item.quality - 1
-  } else {
-    if ($item.quality -lt 50) {
-      $item.quality = $item.quality + 1
-      if ($item.name -eq 'Backstage passes to a TAFKAL80ETC concert') {
-        if ($item.sellIn -lt 11) {
-          if ($item.quality -lt 50) {
-            $item.quality = $item.quality + 1
-          }
-        }
-        if ($item.sellIn -lt 6) {
-          if ($item.quality -lt 50) {
-            $item.quality = $item.quality + 1
-          }
-        }
+  } elseif ($item.quality -lt 50) {
+    $item.quality = $item.quality + 1
+    if ($item.name -eq 'Backstage passes to a TAFKAL80ETC concert') {
+      if ($item.sellIn -lt 11 -and $item.quality -lt 50) {
+        $item.quality = $item.quality + 1
+      }
+      if ($item.sellIn -lt 6 -and $item.quality -lt 50) {
+        $item.quality = $item.quality + 1
       }
     }
   }
@@ -29,19 +23,16 @@ foreach ($item in $items) {
   }
   if ($item.sellIn -lt 0) {
     if ($item.name -ne 'Aged Brie') {
-      if ($item.name -ne 'Backstage passes to a TAFKAL80ETC concert') {
-        if ($item.quality -gt 0) {
-          if ($item.name -ne 'Sulfuras, Hand of Ragnaros') {
-            $item.quality = $item.quality - 1
-          }
-        }
+      if ($item.name -ne 'Backstage passes to a TAFKAL80ETC concert'
+        -and $item.name -ne 'Sulfuras, Hand of Ragnaros'
+        -and $item.quality -gt 0
+      ) {
+        $item.quality = $item.quality - 1
       } else {
         $item.quality = $item.quality - $item.quality
       }
-    } else {
-      if ($item.quality -lt 50) {
-        $item.quality = $item.quality + 1
-      }
+    } elseif ($item.quality -lt 50) {
+      $item.quality = $item.quality + 1
     }
   }
 }
